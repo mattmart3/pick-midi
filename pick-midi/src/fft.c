9fa7ddba7930697e2058b3e83a5ccc341cf64940
matthammers <matteo.martelli9@studio.unibo.it>
2014-02-10 02:20:45 +0100

    License added

--- pick-midi/src/fft.c
+++ pick-midi/src/fft.c
@@ -1,6 +1,23 @@
-/* 2014 (C) Davide Berardi, Matteo Martelli */
-/* A spectrum analizer with fftw3 */
-/* Released under GPLv2 */
+/* 
+ * pick-midi, a guitar to MIDI converter. 
+ * 
+ * Copyright (C) 2014 Davide Berardi, Matteo Martelli.
+ * 
+ * This file is part of pick-midi.
+ * 
+ * pick-midi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * pick-midi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with pick-midi. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* External headers */ 
 #include <stdio.h>
